86d1afb9478aa19d7ab376ef159409c25f75dcb8
MusthafaLuthfi76 <musthafa.luthfi76@gmail.com>
2023-07-01 15:15:00 +0700

    case 5 exit

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -134,7 +134,7 @@ int main()
 
     switch (ch)
     {
-    case '1' :
+    case '1':
     {
         cout << "Enter a word : ";
         string word;
@@ -142,19 +142,22 @@ int main()
         obj.insert(word);
         break;
     }
-    case '2' :
+    case '2':
     {
         obj.inorder(obj.ROOT);
         break;
     }
-    case '3' :
+    case '3':
     {
         obj.preorder(obj.ROOT)};
-        break;
+    break;
     }
-    case '4' :
+    case '4':
     {
         obj.postorder(obj.ROOT);
         break;
+    }
+    case '5' :
+        return 0;
     }
 }
