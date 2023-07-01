98bf588c8325044f9766e3b453120b3b65ff6388
MusthafaLuthfi76 <musthafa.luthfi76@gmail.com>
2023-07-01 15:00:17 +0700

    inorder traversal

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -69,6 +69,21 @@ public :
                 currentNode = currentNode->rightchild;
         }
     }
+
+    void inorder(Node* ptr)
+    {
+        if (ROOT == NULL)
+        {
+            cout << "Tree is Empty" << endl;
+            return;
+        }
+        if (ptr != NULL)
+        {
+            inorder(ptr->leftchild);
+            cout << ptr->info << " ";
+            inorder(ptr->rightchild);
+        }
+    }
 };
 
 int main()
