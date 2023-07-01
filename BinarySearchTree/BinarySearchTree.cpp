4af46ecbc1294470c8e738569d988e5d25aa95f9
MusthafaLuthfi76 <musthafa.luthfi76@gmail.com>
2023-07-01 15:05:23 +0700

    postorder traversal

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -99,6 +99,23 @@ public :
             preorder(ptr->rightchild);
         }
     }
+
+    void postorder(Node* ptr)
+    {
+        // Performs the postorder traversal of the tree
+        if (ROOT == NULL)
+        {
+            cout << "Tree is Empty" << endl;
+            return;
+        }
+        if (ptr != NULL)
+        {
+            postorder(ptr->leftchild);
+            postorder(ptr->rightchild;
+            cout << ptr->info << " ");
+
+        }
+    }
 };
 
 int main()
